87be3dfb60f66ec6310236a3f8177545b7b54624
Jason Mun <30671857+yomun@users.noreply.github.com>
2019-02-16 15:41:33 +0800

    Create snap-install.sh

---
+++ ubuntu/snap-install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+#
+# https://jasonmun.blogspot.my
+# https://github.com/yomun
+# 
+# Copyright (C) 2017 Jason Mun
+# 
+# ---------------------------------------------------------------------------------
+
+sudo snap install sublime-text --classic
+
+sudo snap install vlc
