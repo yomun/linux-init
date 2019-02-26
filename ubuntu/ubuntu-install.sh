7e94c19456f58b023c9be161f8ab535e1f0fe061
Jason Mun <30671857+yomun@users.noreply.github.com>
2019-02-27 05:33:17 +0800

    Update ubuntu-install.sh

--- ubuntu/ubuntu-install.sh
+++ ubuntu/ubuntu-install.sh
@@ -34,6 +34,7 @@ sudo apt install wget -y
 sudo apt install gcc cpp -y
 sudo apt install gnome-tweak-tool -y
 sudo apt install indicator-cpufreq -y
+sudo apt install preload -y
 
 # sudo apt install amule -y
 # sudo apt install bleachbit -y
