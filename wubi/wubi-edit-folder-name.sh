749fddda70433df4ea131959e60807b14556e22c
Jason Mun <30671857+yomun@users.noreply.github.com>
2020-07-18 19:16:18 +0800

    Update wubi-edit-folder-name.sh

--- wubi/wubi-edit-folder-name.sh
+++ wubi/wubi-edit-folder-name.sh
@@ -5,10 +5,10 @@
 # http://jasonmun.blogspot.my/2017/07/ubuntu-wubi.html
 # https://github.com/yomun
 # 
-# Copyright (C) 2017 Jason Mun
+# Copyright (C) 2017 - 2020 Jason Mun
 # 
 
-NEW_FOLDER_NAME="ubuntu_1710"
+NEW_FOLDER_NAME="ubuntu_2004"
 
 cd ~
 
@@ -36,30 +36,30 @@ cp -r build/winboot /host
 
 # --------------------- Windows 7 (修改 Windows Boot Manager 的设置和替换启动文件) ---------------------
 
-# 1) 先将 C:\ubuntu 改名为 C:\ubuntu_1710
+# 1) 先将 C:\ubuntu 改名为 C:\ubuntu_2004
 
-# 2) 用软件 EasyBCD 2.3　查看现在 ubuntu 17.10 所用的 ID
+# 2) 用软件 EasyBCD 2.3　查看现在 ubuntu 20.04 所用的 ID
 # identifier              {905b6225-f81c-11e5-8d07-e79e7f21200d}
 # device                  partition=C:
 # path                    \ubuntu\winboot\wubildr.mbr
-# description             Ubuntu 17.10
+# description             Ubuntu 20.04
 
 # 3) 然后进入 command prompt 输入以下, 修改以上的 path
-# > bcdedit /set {905b6225-f81c-11e5-8d07-e79e7f21200d} path \ubuntu_1710\winboot\wubildr.mbr
+# > bcdedit /set {905b6225-f81c-11e5-8d07-e79e7f21200d} path \ubuntu_2004\winboot\wubildr.mbr
 
-# 4) 将以上制作 C:\winboot　里的 5 个文件, 取代现在原有的 C:\ubuntu_1710\winboot
+# 4) 将以上制作 C:\winboot　里的 5 个文件, 取代现在原有的 C:\ubuntu_2004\winboot
 # 也要将 C:\wubildr 和 C:\wubildr.mbr 更换掉 (是否能进入 Grub 2, 这可是关键..)
 
-# --------------------- Ubuntu 17.10 (进入系统修改设置) ---------------------
+# --------------------- Ubuntu 20.04 (进入系统修改设置) ---------------------
 
 # 1) 进入 Grub 2, 选 ubuntu, 按 e, 进入编辑模式
-# >> 将 2 处路径 /ubuntu/disks/root.disk 改为 /ubuntu_1710/disks/root.disk
+# >> 将 2 处路径 /ubuntu/disks/root.disk 改为 /ubuntu_2004/disks/root.disk
 # >> 将 ro quiet splash $vt_handoff 改为 rw single init=/bin/bash
 # 然后按 ctrl+x 运行
 
 # 2) 进入 ubuntu 后, 将 /etc/fstab 修改下 root.disk 与 swap.disk 的路径
 # sudo vi /etc/fstab
-# /ubuntu/ 改为 /ubuntu_1710/
+# /ubuntu/ 改为 /ubuntu_2004/
 
 # 3) 更新 Grub 2
 # sudo update-grub
